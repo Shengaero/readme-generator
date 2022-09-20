410b59a740bc340799a61e5fb4c097d0984fb899
Kaidan <kaidangustave@yahoo.com>
2022-09-20 17:11:19 -0500

    added additional licenses

--- utils/questions.js
+++ utils/questions.js
@@ -89,7 +89,12 @@ module.exports = [
             createLicense('MIT License', 'https://opensource.org/licenses/MIT', 'License-MIT-yellow.svg'),
             createLicense('BSD 2-Clause License', 'https://opensource.org/licenses/BSD-2-Clause', 'License-BSD_2--Clause-orange.svg'),
             createLicense('BSD 3-Clause License', 'https://opensource.org/licenses/BSD-3-Clause', 'License-BSD_3--Clause-blue.svg'),
-            createLicense('Eclipse Public License 1.0', 'https://opensource.org/licenses/EPL-1.0', 'License-EPL_1.0-red.svg')
+            createLicense('Eclipse Public License 1.0', 'https://opensource.org/licenses/EPL-1.0', 'License-EPL_1.0-red.svg'),
+            createLicense('GNU Affero General Public License v3.0', 'https://www.gnu.org/licenses/agpl-3.0', 'License-AGPL_v3-blue.svg', 'GNU AGPL v3'),
+            createLicense('GNU General Public License v2.0', 'https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html', 'License-GPL_v2-blue.svg', 'GNU GPL v2'),
+            createLicense('Mozilla Public License 2.0', 'https://opensource.org/licenses/MPL-2.0', 'License-MPL_2.0-brightgreen.svg'),
+            createLicense('The Unlicense', 'http://unlicense.org/', 'license-Unlicense-blue.svg'),
+            createLicense('The Do What the Fuck You Want to Public License', 'http://www.wtfpl.net/about/', 'License-WTFPL-brightgreen.svg')
         ]
     }
 ];
